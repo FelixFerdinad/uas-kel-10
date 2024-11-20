44f802528ff4164bcbe4e9bb4e4df9b3eb20e4fb
FelixFerdinad <felixferdinad3@gmail.com>
2024-11-20 14:58:23 +0700

    .

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home; // Ensure to import the Home controller
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HOme::class, 'index'])->name('main');
